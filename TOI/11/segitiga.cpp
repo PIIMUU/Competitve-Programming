5358fc0e4c6fea106687acaa94fa372338a7e9fd
Thammarit Phiwton <42939433+PIIMUU@users.noreply.github.com>
2020-04-06 14:34:10 +0700

    Update segitiga.cpp

--- TOI/11/segitiga.cpp
+++ TOI/11/segitiga.cpp
@@ -3,78 +3,42 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-string s;
+
 bool dp[260][260][3];
-bool vis[260][260][3];
-bool dfs(int l, int r, char need) {
-    if(vis[l][r][need - '0'] == true) {
-        return dp[l][r][need - '0'];
-    }
-    vis[l][r][need - '0'] = true;
-    if(l == r) {
-        if(need == s[l - 1]) {
-            return dp[l][r][need - '0'] = true;
-        } else {
-            return dp[l][r][need - '0'] = false;
-        }
-    }
-    for(int i = l; i <= r; ++i) {
-        if(need == '0') {
-            dp[l][r][0] |= dfs(l, i, '0') & dfs(i + 1, r, '2');
-            if(dp[l][r][0] == true) {
-                break;
-            }
-        } else if(need == '1') {
-            dp[l][r][1] |= dfs(l, i, '0') & dfs(i + 1, r, '1');
-            if(dp[l][r][1] == true) {
-                break;
-            }
-            dp[l][r][1] |= dfs(l, i, '1') & dfs(i + 1, r, '1');
-            if(dp[l][r][1] == true) {
-                break;
-            }
-            dp[l][r][1] |= dfs(l, i, '1') & dfs(i + 1, r, '2');
-            if(dp[l][r][1] == true) {
-                break;
-            }
-            dp[l][r][1] |= dfs(l, i, '2') & dfs(i + 1, r, '0');
-            if(dp[l][r][1] == true) {
-                break;
-            }
-            dp[l][r][1] |= dfs(l, i, '2') & dfs(i + 1, r, '2');
-            if(dp[l][r][1] == true) {
-                break;
-            }
-        } else if(need == '2') {
-            dp[l][r][2] |= dfs(l, i, '0') & dfs(i + 1, r, '0');
-            if(dp[l][r][2] == true) {
-                break;
-            }
-            dp[l][r][2] |= dfs(l, i, '1') & dfs(i + 1, r, '0');
-            if(dp[l][r][2] == true) {
-                break;
-            }
-            dp[l][r][2] |= dfs(l, i, '2') & dfs(i + 1, r, '1');
-        }
-    }
-    return dp[l][r][need - '0'];
-}
+
 int main() {
     ios_base :: sync_with_stdio(0); cin.tie(0);
     int t = 20;
     while(--t > -1) {
+        int n;
+        string s;
         cin >> n >> s;
         for(int i = 0; i <= n; ++i) {
             for(int j = 0; j <= n; ++j) {
                 for(int k = 0; k < 3; ++k) {
                     dp[i][j][k] = false;
-                    vis[i][j][k] = false;
                 }
             }
         }
-        dfs(1, n, '0');
-        if(dp[1][n][0] == true) {
+        for(int i = 0; i < n; ++i) {
+            dp[i][i][s[i] - '0'] = true;
+        }
+        for(int i = 2; i <= n; ++i) {
+            for(int j = 0, k = i - 1; k < n; ++k, ++j) {
+                for(int l = j; l <= k; ++l) {
+                    dp[j][k][0] |= (dp[j][l][0] & dp[l + 1][k][2]);
+                    dp[j][k][1] |= (dp[j][l][0] & dp[l + 1][k][1]);
+                    dp[j][k][1] |= (dp[j][l][1] & dp[l + 1][k][1]);
+                    dp[j][k][1] |= (dp[j][l][1] & dp[l + 1][k][2]);
+                    dp[j][k][1] |= (dp[j][l][2] & dp[l + 1][k][0]);
+                    dp[j][k][1] |= (dp[j][l][2] & dp[l + 1][k][2]);
+                    dp[j][k][2] |= (dp[j][l][0] & dp[l + 1][k][0]);
+                    dp[j][k][2] |= (dp[j][l][1] & dp[l + 1][k][0]);
+                    dp[j][k][2] |= (dp[j][l][2] & dp[l + 1][k][1]);
+                }
+            }
+        }
+        if(dp[0][n - 1][0] == true) {
             cout << "yes\n";
         } else {
             cout << "no\n";
