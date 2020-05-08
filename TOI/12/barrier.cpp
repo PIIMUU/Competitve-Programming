7fddeb07471bd43a54c14ba65a507c8ab0feb7b7
Thammarit Phiwton <42939433+PIIMUU@users.noreply.github.com>
2020-05-08 13:19:49 +0700

    Update barrier.cpp

--- TOI/12/barrier.cpp
+++ TOI/12/barrier.cpp
@@ -3,39 +3,35 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
+long long sum[6000010];
 int main() {
     ios_base :: sync_with_stdio(0); cin.tie(0);
     int n, k;
     cin >> n >> k;
-    vector<int> a(n+1);
-    for(int i = 1;i <= n;i++) {
-        cin >> a[i];
-        a[i] +=a [i - 1];
+    deque<int> dq;
+    long long answer = -1e18;
+    int len;
+    dq.push_back(0);
+    for (int i = 1; i <= n; ++i) {
+        cin >> sum[i];
+        sum[i] += sum[i - 1];
+        while (dq.empty() == false and i - dq.front() > k) {
+            dq.pop_front();
+        }
+        if (sum[i] - sum[dq.front()] > answer) {
+                answer = sum[i] - sum[dq.front()];
+                len = i - dq.front();
+            } else if (sum[i] - sum[dq.front()] == answer) {
+                len = min(len, i - dq.front());
+        }
+        while (dq.empty() == false and sum[i] < sum[dq.back()]) {
+            dq.pop_back();
+        }
+        dq.push_back(i);
     }
-    deque<pair<int, int>> q;
-    q.push_back({0,0});
-    int mx = 0, len = 0;
-    for(int i = 1;i <= n;i++) {
-        while(q.empty() == false) {
-            if(i-q.front().second<=k) {
-                break;
-            }
-            q.pop_front();
-        }
-        if(a[i] - q.front().first > mx) {
-            mx = a[i] - q.front().first;
-            len = i - q.front().second;
-        }
-        else if(mx == a[i] - q.front().first and len > i - q.front().second) {
-            len = i - q.front().second;
-        }
-        while(q.empty() == false) {
-            if(q.back().first >= a[i]) {
-              q.pop_back();
-            }
-            else break;
-        }
-        q.push_back({a[i], i});
+    if (answer <= 0) {
+        cout << 0 << " " << 0;
+    } else {
+        cout << answer << " " << len;
     }
-    cout << mx << '\n' << len << '\n';
 }
